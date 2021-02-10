bce940eb6f321fdcab6a3ac060cc86f5342ec13f
SNarboni <sebastien.narboni@hotmail.fr>
2021-02-10 15:21:17 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Coucou</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
